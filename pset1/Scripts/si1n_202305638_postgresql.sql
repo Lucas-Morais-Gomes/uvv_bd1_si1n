ba606168d51cf8e2baa3342efcad20aac8551ebb
Lucas Morais Gomes <132704058+Lucas-Morais-Gomes@users.noreply.github.com>
2023-05-28 15:51:07 -0300

    Update si1n_202305638_postgresql.sql

--- pset1/Scripts/si1n_202305638_postgresql.sql
+++ pset1/Scripts/si1n_202305638_postgresql.sql
@@ -19,8 +19,8 @@ CREATE 				DATABASE uvv
 					OWNER = 		lucas_morais
 				    TEMPLATE = 		template0
 				    ENCODING =		'UTF8'
-				    LC_COLLATE = 'pt_BR.UTF-8'
-				    LC_CTYPE = 'pt_BR.UTF-8'
+				    LC_COLLATE = 	'pt_BR.UTF-8'
+				    LC_CTYPE =		'pt_BR.UTF-8'
 					ALLOW_CONNECTIONS = 	TRUE;
 				   
 -- Conectando ao Banco de Dados "uvv"
